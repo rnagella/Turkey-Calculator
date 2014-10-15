9729d4d8fbba15b2f77691204485ad9727719493
Ranjith Nagella <nagella.ranjith@gmail.com>
2014-10-14 22:16:22 -0400

    Initial Commit

---
+++ Turkey Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Turkey Calculator
+//
+//  Created by Ranjith Nagella on 10/14/14.
+//  Copyright (c) 2014 Ranjith Nagella. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
